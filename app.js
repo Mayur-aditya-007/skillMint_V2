cc81922e4d41f2c1071ca46f792873170eb9afe7
Mayur Aditya Rawat <mayurrew@gmail.com>
2025-08-23 10:49:51 +0530

    throwing error

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const connectDB = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 
  app.use(cors());
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: true }));
 
+connectDB();
 
 
 app.get('/', (req, res) => {  
-    res.send('Hello, World!');
+    res.send('tis is a backend server');
 });
-
+app.use('/user', userRoutes);
 
 module.exports = app;
